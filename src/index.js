a84bd418872c1dd7b63b1f80dc0a0aa581d0d006
3went2 <webb.png@gmail.com>
2017-11-07 16:25:21 +0800

    add createModels support

--- src/index.js
+++ src/index.js
@@ -10,18 +10,28 @@ const optionsAdapter = (options = {}) => {
 export default class RedisClaypotPlugin {
 	constructor(options = {}) {
 		this._name = options.name || 'redis';
+		this._matchModel = options.matchModel ||
+			((name, key) => (name.toLowerCase() === key.toLowerCase()))
+		;
 	}
 
 	registerDatabase(register) {
 		register(this._name, {
-			connect(options) {
+			connect: (options) => {
 				return new Redis(optionsAdapter(options));
 			},
-			createCache(options) {
+
+			createCache: (options) => {
 				return {
 					...optionsAdapter(options),
 					store: cacheManagerIORedisStore,
 				};
+			},
+
+			createModels: (names, key, options) => {
+				if (names.some((name) => this._matchModel(name, key))) {
+					return { [key]: new Redis(optionsAdapter(options)) };
+				}
 			},
 		});
 	}
